e896b7ea58ae89a953ec5df480be3b32a743c4d4
c0a242182b <c0a242182b@edu.teu.ac.jp>
2025-04-15 16:36:57 +0900

    練習１０：こうかとんの移動

--- pygame_image.py
+++ pygame_image.py
@@ -13,15 +13,26 @@ def main():
     bg_img2 = pg.transform.flip(bg_img, True, False) #練習８
     kk_img = pg.image.load("fig/3.png") #練習２前半
     kk_img = pg.transform.flip(kk_img, True, False) #練習２後半
+    kk_rct = kk_img.get_rect() #練習１０ー１：こうかとんRectの抽出
+    kk_rct.center = 300,200 #練習１０ー２：こうかとんRectの中心座標を設定
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed() #練習１０ー３
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0,1))
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((1,0))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1,0))
         x = tmr #練習６
         screen.blit(bg_img, [-x, 0]) #練習６
         screen.blit(bg_img2, [-x+1600, 0]) # 練習７
         screen.blit(bg_img,[-x+3200,0])
-        screen.blit(kk_img, [300, 200])#練習４
+        screen.blit(kk_img,kk_rct)#練習４
         pg.display.update()
         tmr += 1        
         clock.tick(200)#練習５
