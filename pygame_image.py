c837b80debf980d3736159b75b597701fd3a48c7
c0a242182b <c0a242182b@edu.teu.ac.jp>
2025-04-15 17:57:22 +0900

    演習２

--- pygame_image.py
+++ pygame_image.py
@@ -19,16 +19,17 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        k_lst = [-1,0]
         key_lst = pg.key.get_pressed() #練習１０ー３
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0,-1))
+            k_lst[1] +=-1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0,1))
+            k_lst[1] +=1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((1,0))
+            k_lst[0] +=2
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1,0))
-        kk_rct.move_ip((-1,0))
+            k_lst[0] +=-1
+        kk_rct.move_ip(k_lst)
         x = tmr%3200 #練習６
         screen.blit(bg_img, [-x, 0]) #練習６
         screen.blit(bg_img2, [-x+1600, 0]) # 練習７
